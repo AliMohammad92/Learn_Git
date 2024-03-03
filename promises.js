54c4c7fe94a2c2f948d7fbfae5297709633a7d3d
Ali Mohammad Zaitoun <eng.alimzaitoun@gmail.com>
2024-03-03 22:53:44 +0300

    delete some codes not need

--- promises.js
+++ promises.js
@@ -1,16 +1,3 @@
-// setTimeout(function () {
-// 	document.getElementById("title1").style.visibility = "visible";
-// 	setTimeout(function () {
-// 		document.getElementById("title2").style.visibility = "visible";
-// 		setTimeout(function () {
-// 			document.getElementById("title3").style.visibility = "visible";
-
-// 			setTimeout(function () {
-// 				document.getElementById("title4").style.visibility = "visible";
-// 			}, 1000);
-// 		}, 1000);
-// 	}, 1000);
-// }, 1000);
 new Promise(function (resolve, reject) {
 	setTimeout(() => {
 		document.getElementById("title1").style.visibility = "visible";
